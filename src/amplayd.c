b5ffbd38c55b1fc5a97a7d795375f348c772b64e
Hermann Schwarting <hfs@gmx.de>
2007-02-28 23:02:07 +0000

    André: * Kommandozeilenparameter * libdaemon

--- src/amplayd.c
+++ src/amplayd.c
@@ -19,20 +19,66 @@
  */
 
 #include <blib/blib.h>
+#include <errno.h>
+#include <libdaemon/daemon.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
 #include "config.h"
 #include "playlist.h"
 
+#define AMPLAYD_NAME "amplayd"
 #define AMPLAYD_DEVICE "/dev/am_usb"
 #define AMPLAYD_PLAYLISTDIR "/var/spool/blinken"
 
+static void daemonize( char* name ){
+	pid_t pid;
+
+	daemon_pid_file_ident = daemon_log_ident = daemon_ident_from_argv0(name);
+
+	if ( ( pid = daemon_pid_file_is_running() ) >= 0 ) {
+		daemon_log( LOG_ERR, "%s is already running as PID %u.\n", AMPLAYD_NAME, pid);
+		exit( EXIT_FAILURE );
+	}
+
+	if ( ( pid = daemon_fork() ) < 0 ) {
+		daemon_log( LOG_ERR, "Fork failed.\n");
+		exit( EXIT_FAILURE );
+	} else if ( pid ) { // parent
+		exit( EXIT_SUCCESS );
+	} else { // daemon
+		if ( daemon_pid_file_create() < 0 ) {
+			daemon_log( LOG_ERR, "Could not create PID file (%s).\n", strerror(errno));
+			exit( EXIT_FAILURE );
+		}
+	}
+}
+
 static void usage( const char* name ){
-	g_printerr( "amplayd - Daemon to play movies on an ARCADEmini\n" );
-	g_printerr( "Version %d.%d.%d\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH );
-	g_printerr( "Usage: %s <filename>\n", name );
+	g_printerr( "%s - Daemon to play movies on an ARCADEmini\n", AMPLAYD_NAME );
+	g_printerr( "Usage: %s [OPTIONS] <filename>\n", name );
 	g_printerr( "\n" );
+	g_printerr( "Options:\n" );
+	g_printerr( "\t-d\t\tDebug mode (non-daemon mode).\n" );
+	g_printerr( "\t-h\t\tPrints this help information.\n" );
+	g_printerr( "\t-V\t\tPrints the daemons version.\n" );
+	g_printerr( "\n" );
+}
+
+static void version(){
+	g_printerr( "%s version %d.%d.%d\n", AMPLAYD_NAME, VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH );
+}
+
+void signal_handler(int sig) {
+	switch( sig ){
+		case SIGTERM:
+		case SIGINT:
+		case SIGQUIT:
+			daemon_pid_file_remove();
+			exit( EXIT_SUCCESS );
+	}
 }
 
 gboolean play_movie( GIOChannel *output, BMovie *movie, GError **error ){
@@ -70,29 +116,54 @@ int main( int argc, char *argv[] ){
 	Playlist *playlist = NULL;
 	const gchar *file = NULL;
 	GIOStatus status;
+	int c;
+	char daemonized = 1;
+
+	while ( ( c = getopt( argc, argv, "dhV" ) ) != -1 ) {
+		switch (c) {
+			case 'd':
+				daemonized = 0;
+				break;
+			case 'h':
+				usage( argv[0] );
+				exit( EXIT_SUCCESS );
+				break;
+			case 'V':
+				version();
+				exit( EXIT_SUCCESS );
+		}
+	}
+
+	if ( daemonized ) {
+		daemonize( argv[0] );
+		signal(SIGPIPE, SIG_IGN);
+		signal(SIGTERM, signal_handler);
+		signal(SIGINT, signal_handler);
+		signal(SIGQUIT, signal_handler);
+	}
 
 	b_init();
 
 	playlist = playlist_new( AMPLAYD_PLAYLISTDIR, &error );
 	if( !playlist ){
-		g_printerr( "Error loading playlist directory '"
-				AMPLAYD_PLAYLISTDIR "': %s\n", error->message );
+		g_printerr( "Error loading playlist directory '%s': %s\n", AMPLAYD_PLAYLISTDIR, error->message );
 		g_clear_error( &error );
-		return EXIT_FAILURE;
+		exit( EXIT_FAILURE );
 	}
 	GIOChannel *am_dev = g_io_channel_new_file( AMPLAYD_DEVICE, "w", &error );
 	if( !am_dev ){
-		g_printerr( "Error opening device '" AMPLAYD_DEVICE "': %s\n",
+		g_printerr( "Error opening device '%s': %s\n",
+        AMPLAYD_DEVICE,
 				error->message );
 		g_clear_error( &error );
 		playlist_free( playlist );
-		return EXIT_FAILURE;
+	  exit( EXIT_FAILURE );
 	}
 	if( g_io_channel_set_encoding( am_dev, NULL, &error ) != G_IO_STATUS_NORMAL ){
-		g_printerr( "Can't enable binary mode? %s\n", error->message );
+		g_printerr( "Can't enable binary mode: %s\n", error->message );
 		g_clear_error( &error );
 		playlist_free( playlist );
-		return EXIT_FAILURE;
+		exit( EXIT_FAILURE );
 	}
 
 
@@ -113,4 +184,5 @@ int main( int argc, char *argv[] ){
 
 		play_movie( am_dev, movie, &error );
 	}
+	exit( EXIT_FAILURE );
 }
